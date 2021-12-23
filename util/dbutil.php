fc92b6b6df532f145810eed2a7cd0d83bcfba303
jskami <jskami01@gmail.com>
2021-12-23 21:37:28 +0900

    Revert "htdocs of xampp"
    This reverts commit 048c94ac00467791f78276658473a3cdd711a5b8.

--- util/dbutil.php
+++ util/dbutil.php
@@ -1,19 +1,9 @@
 <?php
 namespace loginex;  // 파일은 열기로 되어 있지만 여기서 만든 것들은 저~기 login.php로 이동한다.
-
-use mysqli;
-
-$HOSTNAME = 'localhost';  // 상수 선언중..
-$USERNAME = 'webapp';  // get connection 하기 위해 작성하는 것이다. 현재 define(정의)한 상태이다.
-$PASSWORD = 'webapp';
-$DATABASENAME = 'webdb';
-// create a connection DB커넥션 설정
-$dbconn = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASENAME); 
-
-if($dbconn) {  // 연결이 되었는지 체크해보자.
-    echo "db연결이 성공하였습니다.";
-} else {
-    die("db연결에 실패하였습니다.". mysqli_connect_error());
+class dbutil {
+    const HOST = 'localhost';  // 상수 선언중..
+    const USERNAME = 'webapp';  // get connection 하기 위해 작성하는 것이다. 현재 define(정의)한 상태이다.
+    const PASSWORD = 'webapp';
+    const DATABASENAME = 'webapp';
 }
-
 ?>
